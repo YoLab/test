e7bfe889bb8e52ae47c71f779ade352d577282a0
Jérôme Bourgeais <bourgeais.jerome@gmail.com>
2012-06-18 12:33:00 +0200

    testing auto login while commiting

--- feat_RSMV-123456_TAG.php
+++ feat_RSMV-123456_TAG.php
@@ -2,4 +2,5 @@
 echo echo "I'm the feature described by the task RSMV-123456 and I've modified once!";
 echo "New modification to test push, pull, merge ....";
 #JB indeed !
+#Testing auto login while commiting
 ?>
